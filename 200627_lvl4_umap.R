7255119e5dc3c5205228bd86f3ac1e1e47c24b18
innesbre <brendan.innes@mail.utoronto.ca>
2020-07-21 09:00:41 -0400

    added lvl4 all gene umaps

--- 200627_lvl4_umap.R
+++ 200627_lvl4_umap.R
@@ -82,3 +82,70 @@ if (file.exists("~/Dropbox/GDB/CMapCorr_files/200630_lvl4_ligUMAP.RData")) {
   }
   save(lvl4_ligUMAP,file="~/Dropbox/GDB/CMapCorr_files/200630_lvl4_ligUMAP.RData")
 }
+
+# WITH ALL GENES ----
+rm(list=ls())
+if (exists("lvl4_data_all")) {
+} else if (file.exists("../CMapCorr_files/lvl4_inputs_allgenes.RData")) {
+  load("../CMapCorr_files/lvl4_inputs_allgenes.RData") 
+} else {
+  source("lvl4_inputs_allgenes.R")
+}
+
+if (file.exists("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_umap_lig16.RData")) {
+  load("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_umap_lig16.RData") 
+} else {
+  temp_param <- umap.defaults
+  temp_param$n_neighbors <- 30
+  temp_param$metric <- "cosine"
+  temp_param$min_dist <- 0.2
+  temp_param$n_epochs <- 500
+  
+  lvl4all_umap_lig16 <- umap(t(lvl4_data_all@mat[,lvl4_data_all@cdesc$pert_iname %in% lig16]),config=temp_param)
+  save(lvl4all_umap_lig16,file="~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_umap_lig16.RData")
+}
+
+
+# UMAP by CT ----
+if (file.exists("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ctUMAP.RData")) {
+  load("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ctUMAP.RData") 
+} else {
+  temp_param <- umap.defaults
+  temp_param$n_neighbors <- 30
+  temp_param$metric <- "cosine"
+  temp_param$min_dist <- 0.2
+  temp_param$n_epochs <- 500
+  
+  lvl4_ctUMAP <- list()
+  for (CT in ct14) {
+    message(paste0(which(CT == ct14),"/",length(ct14)))
+    temp_tx <- rownames(lvl4_data_all@cdesc[lvl4_data_all@cdesc$cell_id == CT &
+                                          lvl4_data_all@cdesc$pert_iname %in% lig16,])
+    if (temp_tx < 1000) {
+      temp_param$n_neighbors <- 5
+    } else {
+      temp_param$n_neighbors <- 30
+    }
+    lvl4_ctUMAP[[CT]] <- umap(t(lvl4_data_all@mat[,temp_tx]),config=temp_param)
+  }
+  save(lvl4_ctUMAP,file="~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ctUMAP.RData")
+}
+
+
+# UMAP per LIG ----
+if (file.exists("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ligUMAP.RData")) {
+  load("~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ligUMAP.RData") 
+} else {
+  lvl4_ligUMAP <- list()
+  temp_param <- umap.defaults
+  temp_param$n_neighbors <- 30
+  temp_param$metric <- "cosine"
+  temp_param$min_dist <- 0.2
+  temp_param$n_epochs <- 500
+  for (LIG in lig16) {
+    message(paste0(which(LIG == lig16),"/",length(lig16)))
+    temp_tx <- rownames(lvl4_data_all@cdesc[lvl4_data_all@cdesc$pert_iname == LIG,])
+    lvl4_ligUMAP[[LIG]] <- umap(t(lvl4_data_all@mat[,temp_tx]),config=temp_param)
+  }
+  save(lvl4_ligUMAP,file="~/Dropbox/GDB/CMapCorr_files/200721_lvl4all_ligUMAP.RData")
+}
---
+++ lvl4_inputs_allgenes.R
@@ -0,0 +1,48 @@
+library(cmapR)
+
+rm(list=ls())
+setwd("~/Dropbox/GDB/CMapCorr/")
+temp <- load("../CMapCorr_files/lvl3_inputs.RData")
+rm(list=ls()[!ls() %in% c("ct14","lig16")])
+
+temp_cmap_path <- "~/Data_LINCS/phase1"
+temp_coldata <- read.table(file.path(temp_cmap_path,"GSE92742_Broad_LINCS_inst_info.txt"),
+                           header=T,sep="\t",row.names=1,colClasses="character",quote="\"")
+# always check # of rows matches .txt with `awk -F "\t" 'END {print NR}' filename`
+# and when it doesn't, make sure R isn't quoting out things based on apostrophes.
+
+lvl4_data_all <- parse.gctx(
+  file.path(temp_cmap_path,"GSE92742_Broad_LINCS_Level4_ZSPCINF_mlr12k_n1319138x12328.gctx"),
+  cid=rownames(temp_coldata)[temp_coldata$pert_type == "trt_lig"])
+temp_id <- lvl4_data_all@cdesc$id
+lvl4_data_all@cdesc <- temp_coldata[temp_id,]
+#fixing fucked-up ligand names (except NRG/ALPHA/BETA)
+lvl4_data_all@cdesc$pert_iname <- toupper(lvl4_data_all@cdesc$pert_iname)
+# fixing floating-point bullshit
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.00999999977648"] <- "0.01"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.029999999329400003"] <- "0.03"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.10000000149"] <- "0.1"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.15000000596"] <- "0.15"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.20000000298"] <- "0.2"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.300000011921"] <- "0.3"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "1.64999997616"] <- "1.65"
+lvl4_data_all@cdesc$pert_dose <- sub("\\.?0+$","",lvl4_data_all@cdesc$pert_dose)
+# fixing dose unit discrepancy
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "0.1" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "100"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "1" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "1000"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "3" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "3000"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "10" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "10000"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "100" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "100000"
+lvl4_data_all@cdesc$pert_dose[lvl4_data_all@cdesc$pert_dose == "300" & 
+                            lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "300000"
+lvl4_data_all@cdesc$pert_dose_unit[lvl4_data_all@cdesc$pert_dose_unit == "ng/ul"] <- "ng/ml"
+lvl4_data_all@cdesc$pert_dose_unit[lvl4_data_all@cdesc$pert_dose_unit == "ng/ml"] <- "ng/mL"
+
+rm(list=grep("^temp",ls(),value=T))
+
+save(list=ls(),file="~/Dropbox/GDB/CMapCorr_files/lvl4_inputs_allgenes.RData")
