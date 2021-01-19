2ecbadcb4169027e0f52186aca455232ba8dc274
innesbre <brendan.innes@mail.utoronto.ca>
2021-01-19 11:13:12 -0500

    tweaked lig295_ligct_pDE

---
+++ temp_paperworksheet.R
@@ -0,0 +1,45 @@
+load("~/Dropbox/GDB_archive/CMapCorr_files/lvl4_inputs_allgenes.RData")
+lvl4_data <- lvl4_data_all
+rm(lvl4_data_all)
+
+temp_ligcountsperct <- sapply(unique(lvl4_data@cdesc$cell_id),function(CT)
+  length(unique(lvl4_data@cdesc[lvl4_data@cdesc$cell_id == CT,"pert_iname"])))
+ct9 <- names(temp_ligcountsperct)[temp_ligcountsperct > 100]
+names(ct9) <- sapply(ct9,function(X) names(ct14)[ct14 == X])
+
+temp_ctcountsperlig <- sapply(unique(lvl4_data@cdesc$pert_iname),function(LIG)
+  length(unique(lvl4_data@cdesc[lvl4_data@cdesc$pert_iname == LIG,"cell_id"])))
+
+lig295 <- names(temp_ctcountsperlig)[temp_ctcountsperlig >= 9]
+
+rm(list=grep("^temp",ls(),value=T))
+
+
+# Unique marker genes per ligand ----
+load("~/Dropbox/GDB_archive/CMapCorr_files/lig295_DE_lig_FDR.RData")
+temp_logica_lig <- FDR_lig <= 0.05
+temp_logica_lig
+temp_genes <- apply(temp_logica_lig,2,which)
+temp_genes
+temp_genes <- temp_genes[sapply(temp_genes,length) > 0]
+temp_genes
+sapply(temp_genes,names)
+temp_genes <- sapply(temp_genes,names)
+
+unique_lig <- sapply(names(temp_genes),function(X) 
+  setdiff(temp_genes[[X]],unlist(temp_genes[names(temp_genes) != X])))
+unique_lig <- unique_lig[sapply(unique_lig,length) > 0]
+
+unique_lig <- sapply(unique_lig,function(X) lvl4_data@rdesc[X,"pr_gene_symbol"])
+
+rm(list=grep("^temp",ls(),value=T))
+
+
+# LigCT ----
+load("~/Dropbox/GDB_archive/CMapCorr_files/lig295_DE_ligct_FDR.RData")
+temp_lig <- sapply(strsplit(colnames(FDR_ligct),"_"),function(X) X[length(X)])
+table(sapply(lig295,function(LIG) sum(FDR_ligct[,temp_lig == LIG] <= 0.05)) > 0)
+table(sapply(lig295,function(LIG) sum(pDE_ligct[temp_lig == LIG,] <= 0.05)) > 0)
+
+
+
